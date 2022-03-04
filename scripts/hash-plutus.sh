48ba8da7cd26a4546dc13e6969ef152bb91a5582
Jonathan Fischoff <jonathangfischoff@gmail.com>
2022-03-03 21:08:52 -0800

    Add addresses

--- scripts/hash-plutus.sh
+++ scripts/hash-plutus.sh
@@ -5,3 +5,8 @@ cardano-cli address build \
   --payment-script-file $thisDir/auction.plutus \
   $BLOCKCHAIN \
   --out-file $thisDir/$BLOCKCHAIN_PREFIX/auction.addr
+
+cardano-cli address build \
+  --payment-script-file $thisDir/escrow.plutus \
+  $BLOCKCHAIN \
+  --out-file $thisDir/$BLOCKCHAIN_PREFIX/escrow.addr
